8338ba8ec112cf2dcf95bc2054d1c4bdde5e217d
Amateur Audio Dude <168192910+AmateurAudioDude@users.noreply.github.com>
2025-05-04 06:42:13 +1000

    v1.0.5

--- NoSleepMobile.js
+++ NoSleepMobile.js
@@ -1,7 +1,7 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
     name: 'No Sleep Mobile',
-    version: '1.0.4',
+    version: '1.0.5',
     author: 'AAD',
     frontEndPath: 'NoSleepMobile/pluginNoSleepMobile.js'
 }
